901f702ccb61f1b4e90eae299588e9174c6062de
Rizito1 <rosio.scabini09@gmail.com>
2025-12-02 17:41:51 -0300

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login.jsx";
 import Register from "./pages/register/register.jsx";
